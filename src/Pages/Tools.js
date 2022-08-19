894f8c22fdddcbed302c6693aef8088b002f9ac9
Ivanka Todorova <fakeheal@gmail.com>
2022-08-19 16:13:23 +0300

    fix: name of link

--- src/Pages/Tools.js
+++ src/Pages/Tools.js
@@ -18,7 +18,7 @@ const Tools = () : React$Element<any> => {
           <div className="flex gap-4 mt-4">
             <a href="https://github.com/fakeheal/conjure-kit#billable-time-tracking-preview"
                className="border-b text-gray-800 font-bold">How to use</a>
-            <a href="https://i.imgur.com/q3uFEJd.png" className="border-b text-gray-800 font-bold">Setup</a>
+            <a href="https://i.imgur.com/q3uFEJd.png" className="border-b text-gray-800 font-bold">Preview</a>
           </div>
         </p>
       </Card>
